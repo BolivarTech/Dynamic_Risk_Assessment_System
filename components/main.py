ba7e3e36d792a434d008ed575e6ebd0f0427db3d
Julian Bolivar <jbolivarg@gmail.com>
2023-06-19 23:26:39 -0400

    Pipeline done

--- components/main.py
+++ components/main.py
@@ -10,12 +10,16 @@ Revision 1.0.0 (2023/06/19): Initial Release
 """
 
 # Main System Imports
-from argparse import ArgumentParser
 import logging as log
 import logging.handlers
 import sys
 import os
 import platform
+import mlflow
+import hydra
+from omegaconf import DictConfig
+import tempfile
+
 
 # Get the running script's path
 RUNNING_PATH = os.path.realpath(os.path.dirname(__file__)) 
@@ -25,39 +29,18 @@ LOGHANDLER = None
 LOGGER = None
 LOGLEVEL_ = logging.INFO
 
+# The steps will be executed on this order
+_steps = [
+    "ingestion",
+    "training",
+    "scoring",
+    "deployment",
+    "reporting"
+]
 
-def build_argparser():
-    """
-    Parse command line arguments.
-
-    :return: command line arguments
-    """
-
-    parser = ArgumentParser(prog="main",
-                            description="Main Pipeline")
-
-
-    parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        default=## INSERT DEFAULT VALUE HERE OR os.path.join(RUNNING_PATH,'## RELATIVE PATH ##'),
-        required=True
-    )
-
-    parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        default=## INSERT DEFAULT VALUE HERE OR os.path.join(RUNNING_PATH,'## RELATIVE PATH ##'),
-        required=True
-    )
-
-
-    return parser.parse_args()
-
-
-def main(args):
+# This automatically reads in the configuration
+@hydra.main(config_path=".", config_name='config', version_base=None)
+def main(config: DictConfig):
     """
     Run the main function
 
@@ -66,9 +49,70 @@ def main(args):
 
     global LOGGER
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    # Get the path at the root of the MLflow project
+    hydra_root_path = hydra.utils.get_original_cwd()
+
+    # Steps to execute
+    steps_par = config['main']['steps']
+    active_steps = steps_par.split(",") if steps_par != "all" else _steps
+
+    # Move to a temporary directory
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        if "ingestion" in active_steps:
+            # Ingest the files
+            _ = mlflow.run(
+                os.path.join(hydra_root_path, "ingestion"),
+                "main",
+                parameters={
+                    "input_path": os.path.join(hydra_root_path, config["ingestion"]["input_folder_path"]),
+                    "out_file": os.path.join(hydra_root_path, config["ingestion"]["output_folder_path"], "finaldata.csv"),
+                    "record_file": os.path.join(hydra_root_path, config["ingestion"]["output_folder_path"], "ingestedfiles.txt"),
+                    "db_file": os.path.join(hydra_root_path, config["database"]["database_folder_path"], "pipeline_data.sqlite")
+                }
+            )
+        if "training" in active_steps:
+            # Train the Model
+            _ = mlflow.run(
+                os.path.join(hydra_root_path, "training"),
+                "main",
+                parameters={
+                    "model_path": os.path.join(hydra_root_path, config["training"]["output_model_path"]),
+                    "db_file": os.path.join(hydra_root_path, config["database"]["database_folder_path"], "pipeline_data.sqlite")
+                }
+            )
+        if "scoring" in active_steps:
+            # Score the model
+            _ = mlflow.run(
+                os.path.join(hydra_root_path, "scoring"),
+                "main",
+                parameters={
+                    "model_file": os.path.join(hydra_root_path, config["training"]["output_model_path"], "trainedmodel.pkl"),
+                    "data_test_file": os.path.join(hydra_root_path, config["diagnostics"]["test_data_path"], "testdata.csv"),
+                    "db_file": os.path.join(hydra_root_path, config["database"]["database_folder_path"], "pipeline_data.sqlite")
+                }
+            )
+        if "deployment" in active_steps:
+            # Deploy the model
+            _ = mlflow.run(
+                os.path.join(hydra_root_path, "deployment"),
+                "main",
+                parameters={
+                    "model_path": os.path.join(hydra_root_path, config["training"]["output_model_path"]),
+                    "record_file": os.path.join(hydra_root_path, config["ingestion"]["output_folder_path"], "ingestedfiles.txt"),
+                    "deploy_path": os.path.join(hydra_root_path, config["production"]["prod_deployment_path"])
+                }
+            )
+        if "reporting" in active_steps:
+            # Generate Model Report
+            _ = mlflow.run(
+                os.path.join(hydra_root_path, "reporting"),
+                "main",
+                parameters={
+                    "model_file": os.path.join(hydra_root_path, config["production"]["prod_deployment_path"], "trainedmodel.pkl"),
+                    "test_data_file": os.path.join(hydra_root_path, config["diagnostics"]["test_data_path"], "testdata.csv"),
+                    "db_file": os.path.join(hydra_root_path, config["database"]["database_folder_path"], "pipeline_data.sqlite")
+                }
+            )
 
 
 if __name__ == '__main__':
@@ -105,6 +149,5 @@ if __name__ == '__main__':
     LOGGER.setLevel(LOGLEVEL_)
     # Start Running
     LOGGER.debug("Running... (001)")
-    args = build_argparser()
-    main(args)
+    main()
     LOGGER.debug("Finished. (001)")
